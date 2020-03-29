7f70a04d1b945e7fc9bf2c6fe78e415874e91f7a
Felipe <felipe@aifund.ai>
2020-03-29 14:15:48 -0500

    adds login functionality

--- src/App.js
+++ src/App.js
@@ -9,14 +9,23 @@ import Reports from "./containers/Reports/Reports"
 import Login from "./containers/Login/Login"
 import CaseDetail from "./containers/CaseDetail/CaseDetail";
 import VideoCall from "./containers/VideoCall/VideoCall"
+import {useAuth0} from "./shared/Auth";
+import NavBar from "./components/NavBar/NavBar";
 
 
 function App() {
+
+    const {loading} = useAuth0()
+
+    if(loading) {
+        return <div>Loading...</div>
+    }
+
     return (
         <div>
             <Router>
                 <Route path={"/"}>
-                    el nav bar
+                    <NavBar/>
                 </Route>
                 <Route path={"/login"} exact>
                     <Login/>
