e9cea848ba1728eb37f36b6aac704759d24cac16
jpvg10 <juan.valencia10a@gmail.com>
2020-03-29 15:04:53 -0500

    Cases styling

--- src/components/NameAgeCard/NameAgeCard.js
+++ src/components/NameAgeCard/NameAgeCard.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import Card from "react-bootstrap/Card";
-
 import Accordion from 'react-bootstrap/Accordion';
+import styled from 'styled-components';
 
-const NameAgeCard = ({name="Juan Sebastian Jaramillo", age="", id_num=""}) => {
-    //debugger;
+const NameAgeCard = ({name="Juan Sebastian Jaramillo", age="", id_num="", className}) => {
     return (
-            <Accordion defaultActiveKey="0">
-              <Card>
-                <Accordion.Toggle as={Card.Header} eventKey="0">
-                  {name} (Edad: {age} años)
-                </Accordion.Toggle>
-                <Accordion.Collapse eventKey="0">
-                  <Card.Body>Numero de Identificacion: {id_num}</Card.Body>
-                </Accordion.Collapse>
-              </Card>
-            </Accordion>
+        <Accordion defaultActiveKey="0" className={className}>
+          <Card>
+            <Accordion.Toggle as={Card.Header} eventKey="0">
+              {name} (Edad: {age} años)
+            </Accordion.Toggle>
+            <Accordion.Collapse eventKey="0">
+              <Card.Body>Numero de Identificacion: {id_num}</Card.Body>
+            </Accordion.Collapse>
+          </Card>
+        </Accordion>
     );
 };
 
@@ -23,4 +22,8 @@ NameAgeCard.propTypes = {
 
 };
 
-export default NameAgeCard;
+const StyledCard = styled(NameAgeCard)`
+  margin-bottom: 20px;
+`;
+
+export default StyledCard;
--- src/containers/CaseDetail/CaseDetail.js
+++ src/containers/CaseDetail/CaseDetail.js
@@ -7,32 +7,9 @@ import UserDetailedSymptomsCard from "../../components/UserDetailedSymptomsCard/
 import CasesAPI from "../../services/CasesAPI";
 import {useParams} from "react-router";
 import ReportsAPI from "../../services/ReportsAPI";
-import styled from "styled-components";
-
-const CaseDetailContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin: 0 50px;
-`
-
-const GridContainer = styled.div`
-  display: grid;
-  grid-gap: 10px;
-  grid-template-columns: 50% auto;
-  grid-template-areas:
-    "user-info user-info"
-    "report-side diagnosis"
-    "report-side contact-info"
-`
-
-const UserInfoContainer = styled.div`
-  grid-area: user-info;
-`
-
-const ReportSideContainer = styled.div`
-  grid-area: report-side;
-`
-
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const CaseDetail = () => {
     //TODO: LOAD FROM HISTORY PROPS THE PATH to get the caseId
@@ -58,15 +35,19 @@ const CaseDetail = () => {
     }, [caseId])
 
     return (
-        <CaseDetailContainer>
+        <Container>
             <Timeline items={caseState}/>
-            <GridContainer>
-                <UserInfoContainer><NameAgeCard name={report.name} age={report.age} id_num={report.userId}/></UserInfoContainer>
-                <ReportSideContainer><UserDetailedSymptomsCard/></ReportSideContainer>
-                <DiagnosticCard style={{gridAre:"diagnosis"}} onDiagnosisChange={(diagnosis) => console.log(diagnosis.target.value)}/>
-                <ContactDataCard style={{gridAre:"contact-info"}} phone={report.phone}/>
-            </GridContainer>
-        </CaseDetailContainer>
+            <NameAgeCard name={report.name} age={report.age} id_num={report.userId}/>
+            <Row>
+                <Col>
+                    <UserDetailedSymptomsCard />
+                </Col>
+                <Col>
+                    <DiagnosticCard style={{gridAre:"diagnosis"}} onDiagnosisChange={(diagnosis) => console.log(diagnosis.target.value)}/>
+                    <ContactDataCard style={{gridAre:"contact-info"}} phone={report.phone}/>
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
