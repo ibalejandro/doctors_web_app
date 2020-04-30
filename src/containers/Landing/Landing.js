93ca73e9a6126984cb84a4f122c919ca771048e6
ibalejandro <asanch41@eafit.edu.co>
2020-04-30 01:06:34 -0500

    Adding the log in button to the landing page.

--- src/containers/Landing/Landing.js
+++ src/containers/Landing/Landing.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, {useEffect} from "react";
 import DoctorRegistration from "../DoctorRegistration/DoctorRegistration";
 import GeneralInformation from "../../components/GeneralInformation/GeneralInformation";
+import LoginRedirection from "../LoginRedirection/LoginRedirection";
+import {useAuth0} from "../../shared/Auth";
+import {useHistory} from "react-router-dom";
 
 const Landing = () => {
+
+    const {loading, isAuthenticated, token} = useAuth0();
+    let history = useHistory();
+
+    useEffect(() => {
+        if (isAuthenticated && token) {
+            history.push("/reports");
+        }
+    }, [isAuthenticated, token]);
+
+    if (loading || (isAuthenticated && token)) return <div>Cargando...</div>;
+
     return (
         <div>
             <GeneralInformation/>
+            <LoginRedirection/>
             <DoctorRegistration/>
         </div>
     );
